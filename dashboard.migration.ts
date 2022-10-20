8925db826de3d7baa766ec83a732455257bfd4e8
Sanzhar <sanzhar.danybaev@turing.com>
2022-10-21 00:17:20 +0600

    Implement datasource change algorithm

--- dashboard.migration.ts
+++ dashboard.migration.ts
@@ -11,9 +11,28 @@ if(!file || !datasource || !uuid) {
 }
 
 try{
-const fileContent = fs.readFileSync(path.resolve(file), 'utf-8')
 
-console.log(fileContent)
+const filePath = path.resolve(file)
+
+const fileContent = fs.readFileSync(filePath, 'utf-8')
+
+const parsedJSON = JSON.parse(fileContent)
+
+if(!parsedJSON.panels) {
+    console.error('Provided file is not a valid dashboard')
+    process.exit()
+}
+
+parsedJSON.panels.forEach((panel: any) => {
+    panel.datasource.type = datasource
+    panel.datasource.uid = uuid 
+})
+
+
+fs.writeFileSync(filePath,JSON.stringify(parsedJSON),{encoding:'utf8',flag:'w'})
+
+console.log('Successfully updated datasource')
+
 } catch(e: any){
     console.error(e.message)
 }
