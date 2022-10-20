11013dc5c080da15b1b63e5d282a6ef4f8369405
Sanzhar <sanzhar.danybaev@turing.com>
2022-10-20 23:59:53 +0600

    Added arguments validation

--- dashboard.migration.ts
+++ dashboard.migration.ts
@@ -1,1 +1,11 @@
 import fs from 'fs'
+import path from 'path'
+import { argv } from 'process'
+
+
+const [datasource, uuid] = argv.slice(2)
+
+if(!datasource || !uuid) {
+  console.error('You must provide a datasource and a uuid')
+  process.exit(1)
+}
