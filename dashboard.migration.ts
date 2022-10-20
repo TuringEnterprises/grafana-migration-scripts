9b6f5d0fb840b73f1ea3d34f4e85d52156eed566
sanzhar-dan <sanzhar.danybaev@turing.com>
2022-10-21 02:55:22 +0600

    Feature upgrade

--- dashboard.migration.ts
+++ dashboard.migration.ts
@@ -5,37 +5,47 @@ import { argv } from 'process'
 
 const [file, datasource, uuid] = argv.slice(2)
 
-if(!file || !datasource || !uuid) {
-  console.error('You must provide a fle, datasource and a uuid')
-  process.exit(1)
+if (!file || !datasource || !uuid) {
+    console.error('You must provide a fle, datasource and a uuid')
+    process.exit(1)
 }
 
-try{
+try {
 
-const filePath = path.resolve(file)
+    const filePath = path.resolve(file)
 
-const fileContent = fs.readFileSync(filePath, 'utf-8')
+    const fileContent = fs.readFileSync(filePath, 'utf-8')
 
-const parsedJSON = JSON.parse(fileContent)
+    const parsedJSON = JSON.parse(fileContent)
 
-if(!parsedJSON.panels) {
-    console.error('Provided file is not a valid dashboard')
-    process.exit()
-}
-
-parsedJSON.panels.forEach((panel: any) => {
-    if(panel.datasource?.type === 'prometheus'){
-        panel.datasource.type = datasource
-        panel.datasource.uid = uuid 
+    if (!parsedJSON.panels) {
+        console.error('Provided file is not a valid dashboard')
+        process.exit()
     }
-})
+
+    parsedJSON.panels.forEach((panel: any) => {
+        if (panel.datasource?.type === 'prometheus') {
+            panel.datasource.type = datasource
+            panel.datasource.uid = uuid
+        }
+
+        if (panel.targets) {
+            panel.targets.forEach((target: any) => {
+                if (target.datasource?.type === 'prometheus') {
+                    target.datasource.type = datasource
+                    target.datasource.uid = uuid
+                }
+            })
+        }
+
+    })
 
 
-fs.writeFileSync(filePath,JSON.stringify(parsedJSON),{encoding:'utf8',flag:'w'})
+    fs.writeFileSync(filePath, JSON.stringify(parsedJSON), { encoding: 'utf8', flag: 'w' })
 
-console.log('Successfully updated datasource')
+    console.log('Successfully updated datasource')
 
-} catch(e: any){
+} catch (e: any) {
     console.error(e.message)
 }
 
