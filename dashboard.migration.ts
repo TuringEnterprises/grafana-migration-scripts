581db94b5fce9cc4fd8b1e373efe6f9564bd4f98
Sanzhar <sanzhar.danybaev@turing.com>
2022-10-21 00:00:22 +0600

    Added file validation

--- dashboard.migration.ts
+++ dashboard.migration.ts
@@ -3,9 +3,10 @@ import path from 'path'
 import { argv } from 'process'
 
 
-const [datasource, uuid] = argv.slice(2)
+const [file, datasource, uuid] = argv.slice(2)
 
-if(!datasource || !uuid) {
-  console.error('You must provide a datasource and a uuid')
+if(!file || !datasource || !uuid) {
+  console.error('You must provide a fle, datasource and a uuid')
   process.exit(1)
 }
+
